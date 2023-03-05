02d2269d68ff936557b806948b31d08fa02ffdcd
lelong181 <longld2284@gmail.com>
2023-03-05 23:37:29 +0700

    Update namespace

--- Assets/Scripts/Item/MysteriousPotion.cs
+++ Assets/Scripts/Item/MysteriousPotion.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace Item ;
 
 public class MysteriousPotion : MonoBehaviour
 {
--- Assets/Scripts/Item/Timer.cs
+++ Assets/Scripts/Item/Timer.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+namespace Item ;
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
