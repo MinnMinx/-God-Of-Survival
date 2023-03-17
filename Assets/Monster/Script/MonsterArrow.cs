97c31748b6514528dbeb6d799c4d744f626bd815
Duoc Nguyen <duocng.2409@gmail.com>
2023-03-17 08:32:49 +0700

    commit range monster

--- Assets/Monster/Script/MonsterArrow.cs
+++ Assets/Monster/Script/MonsterArrow.cs
@@ -6,11 +6,13 @@ namespace Monster
 {
     public class MonsterArrow : Monster
     {
+        [SerializeField]
         private GameObject arrow;
+        [SerializeField]
+        private float bulletSp = 7f;
 
         private new void Start()
         {
-            arrow = GetComponent<GameObject>();
             base.Start();
             Atk = 3;
             Basehp = 3;
@@ -22,6 +24,14 @@ namespace Monster
         }
         public override void Attack()
         {
+            Vector2 target = new Vector2(Des.position.x, Des.position.y);
+            Vector2 direction = (target - (Vector2)transform.position).normalized;
+            var newBullet = Instantiate(arrow, transform.position, Quaternion.Euler(0, 0, -Vector2.SignedAngle(direction, Vector2.up)));
+            var rb = newBullet.GetComponent<Rigidbody2D>();
+            newBullet.GetComponent<BulletController>().Atk = Atk;
+            //newBullet.transform.LookAt(direction);
+            rb.AddForce(direction * bulletSp, ForceMode2D.Impulse);
+            base.Cd = 0;    
             Debug.Log("Arrow Attack");
         }
     }
