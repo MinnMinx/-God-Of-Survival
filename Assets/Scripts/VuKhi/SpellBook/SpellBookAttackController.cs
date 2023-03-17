b698af38cfb54b71f2a0c14aa2207e162692095a
Minnminx <theultimatrix2@gmail.com>
2023-03-17 09:47:28 +0700

    Adding stuff to Spellbook

---
+++ Assets/Scripts/VuKhi/SpellBook/SpellBookAttackController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VuKhi
+{
+	[RequireComponent(typeof(ParticleSystem))]
+	public class SpellBookAttackController : MonoBehaviour
+	{
+		[SerializeField]
+		private SpellBookController _parent;
+		private ParticleSystem ps;
+		private List<ParticleCollisionEvent> collidedParticles;
+
+		private void Start()
+		{
+			ps = GetComponent<ParticleSystem>();
+			collidedParticles = new List<ParticleCollisionEvent>(ps.main.maxParticles);
+		}
+
+		private void OnParticleCollision(GameObject other)
+		{
+			if (_parent == null)
+				return;
+
+			int numCollisionEvents = ps.GetCollisionEvents(other, collidedParticles);
+			Monster.Monster monster = null;
+			while (numCollisionEvents-- > 0)
+			{
+				monster = collidedParticles[numCollisionEvents].colliderComponent.GetComponent<Monster.Monster>();
+				if (monster != null)
+				{
+					monster.takedamage(_parent.ATKBase);
+					if (monster != null && monster.Hp <= 0)
+					{
+						Debug.Log("WTF " + monster.gameObject.name);
+						Destroy(monster.gameObject);
+					}
+				}
+			}
+		}
+	}
+}
