f304938887c1b5e50175ce98187fe22bfa557c9c
Duoc Nguyen <duocng.2409@gmail.com>
2023-03-17 09:02:11 +0700

    update range monster

--- Assets/Monster/Script/MonsterMage.cs
+++ Assets/Monster/Script/MonsterMage.cs
@@ -6,6 +6,13 @@ namespace Monster
 {
     public class MonsterMage : Monster
     {
+        [SerializeField]
+        private GameObject bullet;
+
+        [SerializeField]
+        private float bulletSp = 10f;
+
+        private int numbers = 3;
         private new void Start()
         {
             base.Start();
@@ -20,7 +27,17 @@ namespace Monster
 
         public override void Attack()
         {
-            Debug.Log("Mage Attack");
+            Vector2 target = new Vector2(Des.position.x, Des.position.y);
+            for (int i = 0;i < numbers; i++)
+            {
+                var newBullet = Instantiate(bullet, transform.position, transform.rotation);
+                var rb = newBullet.GetComponent<Rigidbody2D>();
+                newBullet.GetComponent<BulletController>().Atk = Atk;
+                Vector2 direction = (target - (Vector2)transform.position).normalized;
+                rb.AddForce(direction * bulletSp, ForceMode2D.Impulse);
+            }           
+            base.Cd = 0;
+            Debug.Log("Gun Attack");
         }
     }
 }
